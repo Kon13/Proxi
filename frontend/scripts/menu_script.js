fa38e4ee7db7c9878448d0de82add7e6d3e0c0ed
Nicolas Ryjoukhine <nicolasryjou@gmail.com>
2021-11-08 23:37:08 +0800

    Working on G-Maps. Created a template HTML page

---
+++ frontend/scripts/menu_script.js
@@ -0,0 +1,30 @@
+let map;
+
+
+function getLocation() {
+  if (navigator.geolocation) {
+    return navigator.geolocation.getCurrentPosition(_coords_read);
+  } else { 
+    alert("Sorry, but we cannot access your geolocation. Please try again later");
+  }
+}
+
+function _coords_read(){
+    lat_long = {
+      "lat":position.coords.latitude,
+      "long":position.coords.longitude
+    }
+    return lat_long;
+}
+
+function display_usr_loc(location, zoom_num, map_container){
+  map = new google.maps.Map(map_container, {
+    center: { lat: location.lat, lng: location.long },
+    zoom: zoom_num,
+  });
+}
+// TEST
+let current_location = getLocation();
+let map_div = document.getElementById("maps");
+displayLocation(current_location, 8, map_div);
+// TEST
